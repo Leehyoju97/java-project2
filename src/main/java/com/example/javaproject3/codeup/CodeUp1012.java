3ee385459f837fc97ff588b36345bf2f345302cc
Leehyoju97 <dlgywn2420@gmail.com>
2023-04-20 11:50:58 +0900

    feat : codeup 1012, 1013

---
+++ src/main/java/com/example/javaproject3/codeup/CodeUp1012.java
@@ -0,0 +1,14 @@
+package com.example.javaproject3.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1012 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        float x = sc.nextFloat();
+
+        System.out.printf("%05f", x);
+    }
+}
