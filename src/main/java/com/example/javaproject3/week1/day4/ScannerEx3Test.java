19e43e5fb1cee5539985937bcba8e970e0f92f60
Leehyoju97 <dlgywn2420@gmail.com>
2023-04-20 10:43:31 +0900

    feat : 두 숫자 입력하고 출력

---
+++ src/main/java/com/example/javaproject3/week1/day4/ScannerEx3Test.java
@@ -0,0 +1,9 @@
+package com.example.javaproject3.week1.day4;
+
+public class ScannerEx3Test {
+
+    public static void main(String[] args) {
+        ScannerEx3 ex3 = new ScannerEx3();
+        ex3.inputTwoNumbersAndPlus();
+    }
+}
