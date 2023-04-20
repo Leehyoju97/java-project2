d733d1dcd119a11a1b8e2fe4e77ea08923c7d0c3
Leehyoju97 <dlgywn2420@gmail.com>
2023-04-20 11:57:50 +0900

    feat : printf로 날짜 출력

---
+++ src/main/java/com/example/javaproject3/week1/day4/Calendar.java
@@ -0,0 +1,12 @@
+package com.example.javaproject3.week1.day4;
+
+public class Calendar {
+
+    public static void main(String[] args) {
+        int year = 2023;
+        int month = 4;
+        int day = 20;
+
+        System.out.printf("%d-%02d-%02d", year, month, day);
+    }
+}
