df7f1325318386400b80c3dfe80f0fce0cf10c79
Leehyoju97 <dlgywn2420@gmail.com>
2023-04-20 13:11:44 +0900

    feat : CodeUp1014, 1015 풀이

---
+++ src/main/java/com/example/javaproject3/codeup/CodeUp1014.java
@@ -0,0 +1,16 @@
+package com.example.javaproject3.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1014 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String x = sc.next();
+        String y = sc.next();
+
+        System.out.printf("%s %s", y, x);
+    }
+}
