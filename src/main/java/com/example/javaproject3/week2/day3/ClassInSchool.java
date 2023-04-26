56073d9a412bbe026009893ca09a2d88b3f13924
Leehyoju97 <dlgywn2420@gmail.com>
2023-04-26 10:07:27 +0900

    feat : 클래스 배열 실습

---
+++ src/main/java/com/example/javaproject3/week2/day3/ClassInSchool.java
@@ -0,0 +1,10 @@
+package com.example.javaproject3.week2.day3;
+
+public class ClassInSchool {
+
+    int no;
+    String teacherName;
+    Student[] students;
+    Teacher teacher;
+
+}
---
+++ src/main/java/com/example/javaproject3/week2/day3/ClassInSchoolTest.java
@@ -0,0 +1,17 @@
+package com.example.javaproject3.week2.day3;
+
+public class ClassInSchoolTest {
+
+    public static void main(String[] args) {
+        ClassInSchool classInSchool = new ClassInSchool();
+        classInSchool.no = 1;
+        classInSchool.teacher = new Teacher();
+        classInSchool.teacher.name = "이효주";
+        classInSchool.teacher.age = 33;
+        classInSchool.teacher.address = "에버랜드";
+        classInSchool.students = new Student[30];
+
+        System.out.println(classInSchool.teacher.name);
+        System.out.printf("%d반 담임 선생님 성함은 %s입니다.", classInSchool.no, classInSchool.teacher.name);
+    }
+}
