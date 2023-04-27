950291957d5b67b5e09f111323d161aa9a2b3721
Leehyoju97 <dlgywn2420@gmail.com>
2023-04-27 10:19:11 +0900

    feat : 증감연산자

---
+++ src/main/java/com/example/javaproject3/week2/day4/ComparisonOperatorsEx.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.week2.day4;
+
+public class ComparisonOperatorsEx {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        boolean result = a == b;
+        System.out.printf("a와 b가 같은지? %b", result);
+    }
+}
+
