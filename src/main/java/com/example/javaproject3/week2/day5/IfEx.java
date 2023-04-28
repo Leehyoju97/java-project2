ea191cf57fc33d84caf1f2ad5f1725e9661cf7b1
Leehyoju97 <dlgywn2420@gmail.com>
2023-04-28 09:17:29 +0900

    feat : 조건문 실습

---
+++ src/main/java/com/example/javaproject3/week2/day5/IfEx.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.week2.day5;
+
+public class IfEx {
+
+    public static void main(String[] args) {
+
+        if(true) {
+            System.out.println("실행");
+        }
+    }
+}
