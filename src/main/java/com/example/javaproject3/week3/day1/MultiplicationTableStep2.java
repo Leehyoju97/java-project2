b930ed15bf00fba8c6c80b80ff9714ca87b2d83b
Leehyoju97 <dlgywn2420@gmail.com>
2023-05-01 14:23:37 +0900

    feat : 구구단

--- src/main/java/com/example/javaproject3/week3/day1/MultiplicationTableStep2.java
+++ src/main/java/com/example/javaproject3/week3/day1/MultiplicationTableStep2.java
@@ -5,6 +5,14 @@ public class MultiplicationTableStep2 {
         for (int i = 1; i <= 9; i++) {
             System.out.printf("2 * %d = %d\n", i, 2 * i);
         }
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("3 * %d = %d\n", i, 3 * i);
+        }
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("4 * %d = %d\n", i, 4 * i);
+        }
+
     }
 }
 
