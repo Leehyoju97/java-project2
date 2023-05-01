2084ae363e7fe893c6ef66da400836b4a930cd7a
Leehyoju97 <dlgywn2420@gmail.com>
2023-05-01 11:51:48 +0900

    feat : for문 출력

---
+++ src/main/java/com/example/javaproject3/week3/day1/ForLoopForwardStep.java
@@ -0,0 +1,10 @@
+package com.example.javaproject3.week3.day1;
+
+public class ForLoopForwardStep {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
+
