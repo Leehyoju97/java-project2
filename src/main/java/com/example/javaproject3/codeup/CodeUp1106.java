f40ca761557ddf71904808528c2c631ef4bb8abf
Leehyoju97 <dlgywn2420@gmail.com>
2023-05-01 11:27:37 +0900

    feat : codeUp1106

---
+++ src/main/java/com/example/javaproject3/codeup/CodeUp1106.java
@@ -0,0 +1,12 @@
+package com.example.javaproject3.codeup;
+
+public class CodeUp1106 {
+
+    public static void main(String[] args) {
+
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+
+        System.out.printf("%d %d", min, max);
+    }
+}
