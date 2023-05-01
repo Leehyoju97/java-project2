c326cda77f7a305ebf55b76d7c8ea99284d5cee6
Leehyoju97 <dlgywn2420@gmail.com>
2023-05-01 11:51:24 +0900

    feat : codeup1109 - codeup1115

---
+++ src/main/java/com/example/javaproject3/codeup/CodeUp1109.java
@@ -0,0 +1,19 @@
+package com.example.javaproject3.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1109 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.next();
+        int age = sc.nextInt();
+        String departmentcode = sc.next();
+        double secretKey = sc.nextDouble();
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(departmentcode);
+        System.out.println(secretKey);
+    }
+}
---
+++ src/main/java/com/example/javaproject3/codeup/CodeUp1115.java
@@ -0,0 +1,16 @@
+package com.example.javaproject3.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1115 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long num1 = sc.nextLong();
+        long num2 = sc.nextLong();
+        long result = num1 + num2;
+
+        System.out.println(result);
+    }
+}
