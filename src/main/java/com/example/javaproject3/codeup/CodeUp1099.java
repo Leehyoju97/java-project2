f930bcca7b28a1c3d917dee8f1432b73b18311a2
Leehyoju97 <dlgywn2420@gmail.com>
2023-05-04 14:47:26 +0900

    fix : codeup1099 리팩토링

--- src/main/java/com/example/javaproject3/codeup/CodeUp1099.java
+++ src/main/java/com/example/javaproject3/codeup/CodeUp1099.java
@@ -4,14 +4,12 @@ import java.util.Scanner;
 
 public class CodeUp1099 {
 
-    private static final int num = 10;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] arr = new int[num][num];
+        int[][] arr = new int[10][10];
 
-        for (int i = 0; i < num; i++) {
-            for (int j = 0; j < num; j++) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
@@ -21,24 +19,35 @@ public class CodeUp1099 {
 
         while (true) {
 
+            // 먹이를 찾을 경우
             if (arr[xIndex][yIndex] == 2) {
                 arr[xIndex][yIndex] = 9;
                 break;
-            } else if (arr[xIndex][yIndex] == 0) {
-                arr[xIndex][yIndex] = 9;
             }
 
-            if (arr[xIndex][yIndex + 1] == 0) {
+            arr[xIndex][yIndex] = 9;
+
+            // 옆으로 갈 수 있는 경우
+            if (arr[xIndex][yIndex + 1] == 0 || arr[xIndex][yIndex + 1] == 2) {
                 yIndex++;
+                continue;
+
+                // 옆이 막힌 경우
             } else if (arr[xIndex][yIndex + 1] == 1) {
-                xIndex++;
+                // 아래로 내려가야 되는 경우
+                if (arr[xIndex + 1][yIndex] == 0 || arr[xIndex + 1][yIndex] == 2) {
+                    xIndex++;
+                    continue;
+
+                    // 밑에도 막혀있는 경우
+                } else if (arr[xIndex + 1][yIndex] == 1) {
+                    break;
+                }
             }
         }
 
-        arr[xIndex][yIndex] = 9;
-
-        for (int i = 0; i < num; i++) {
-            for (int j = 0; j < num; j++) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
                 System.out.printf("%d ", arr[i][j]);
             }
 
