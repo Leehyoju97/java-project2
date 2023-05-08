9a3bb3c968fa45ebbef279e07f5515974b89d2a5
Leehyoju97 <dlgywn2420@gmail.com>
2023-05-08 10:27:23 +0900

    feat : 별 계단 BufferedWriter 사용풀이

--- src/main/java/com/example/javaproject3/codeup/CodeUp1361.java
+++ src/main/java/com/example/javaproject3/codeup/CodeUp1361.java
@@ -1,19 +1,22 @@
 package com.example.javaproject3.codeup;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class CodeUp1361 {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        int height = 5;
 
-        int n = sc.nextInt();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.printf(" ");
-            }
-            System.out.println("**");
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < i; j++) bw.append(" ");
+            bw.append("**\n");
         }
+        bw.flush();
+        bw.close();
     }
+
 }
