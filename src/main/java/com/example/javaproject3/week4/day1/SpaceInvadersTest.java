fffd4c91bf15788e9fdcd6a8c89ebf4fd85a5536
Leehyoju97 <dlgywn2420@gmail.com>
2023-05-08 14:56:40 +0900

    feat : 수업 실습

---
+++ src/main/java/com/example/javaproject3/week4/day1/SpaceInvadersTest.java
@@ -0,0 +1,12 @@
+package com.example.javaproject3.week4.day1;
+
+public class SpaceInvadersTest {
+    public static void main(String[] args) {
+        SpaceInvaders si = new SpaceInvaders();
+        si.moveLeft();
+        System.out.println(si.location);
+        si.moveRight();
+        System.out.println(si.location);
+    }
+}
+
