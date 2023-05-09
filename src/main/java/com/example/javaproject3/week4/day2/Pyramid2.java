05572076c05137151d0bc5ca42b0e7c721de2574
Leehyoju97 <dlgywn2420@gmail.com>
2023-05-09 09:49:22 +0900

    refactor :  피라미드 생성자 이용, 메소드를 기능 분리

--- src/main/java/com/example/javaproject3/week4/day2/Pyramid2.java
+++ src/main/java/com/example/javaproject3/week4/day2/Pyramid2.java
@@ -7,22 +7,22 @@ public class Pyramid2 {
         this.spaceChar = spaceChar;
     }
 
-    public String makeALine(String spaceChar, int height, int i) {
+    public String makeALine(int height, int i) {
         return String.format("%s%s\n", spaceChar.repeat(height - i - 1), "*".repeat(2 * i + 1));
     }
 
-    public void printPyramidWithSpaceChar(int height, String spaceChar) {
+    public void printPyramidWithSpaceChar(int height) {
         // 기능이 3가지
         for (int i = 0; i < height; i++) {
-            System.out.print(makeALine(spaceChar, height, i));
+            System.out.print(makeALine(height, i));
         }
     }
 
     public static void main(String[] args) {
         Pyramid2 pyramid2 = new Pyramid2(" ");
         Pyramid2 pyramidSpaceZero = new Pyramid2("0");
-        pyramid2.printPyramidWithSpaceChar(4, pyramid2.spaceChar);
-        pyramidSpaceZero.printPyramidWithSpaceChar(4, pyramidSpaceZero.spaceChar);
+        pyramid2.printPyramidWithSpaceChar(4);
+        pyramidSpaceZero.printPyramidWithSpaceChar(4);
     }
 }
 
