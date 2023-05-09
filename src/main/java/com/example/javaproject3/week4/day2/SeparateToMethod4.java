91553078ea9dc4cfdea8b4608a125289f14db4d0
Leehyoju97 <dlgywn2420@gmail.com>
2023-05-09 11:40:47 +0900

    refactor :  static 메소드 실습

--- src/main/java/com/example/javaproject3/week4/day2/SeparateToMethod4.java
+++ src/main/java/com/example/javaproject3/week4/day2/SeparateToMethod4.java
@@ -7,6 +7,7 @@ public class SeparateToMethod4 {
         System.out.println(Arrays.toString(arr[0]));
         System.out.println(Arrays.toString(arr[1]));
         System.out.println(Arrays.toString(arr[2]));
+        System.out.println("-----------");
     }
 
     public static void main(String[] args) {
@@ -16,13 +17,10 @@ public class SeparateToMethod4 {
                 {70, 80, 90}
         };
         printArray(arr);
-
-        System.out.println("-----------");
         arr[0][0] = 0;
         arr[1][1] = 0;
         arr[2][2] = 0;
         printArray(arr);
-        System.out.println("-----------");
         arr[0][2] = 0;
         arr[1][1] = 0;
         arr[0][2] = 0;
