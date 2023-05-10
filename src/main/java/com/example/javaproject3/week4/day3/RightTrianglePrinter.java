7a9b8787ba37ad49eee693ca6d3531931aa8d545
Leehyoju97 <dlgywn2420@gmail.com>
2023-05-10 13:58:10 +0900

    feat : 인터페이스를 통해 파일에 별찍기 추가, 리펙토링

--- src/main/java/com/example/javaproject3/week4/day3/RightTrianglePrinter.java
+++ src/main/java/com/example/javaproject3/week4/day3/RightTrianglePrinter.java
@@ -1,7 +1,5 @@
 package com.example.javaproject3.week4.day3;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class RightTrianglePrinter {
@@ -11,30 +9,8 @@ public class RightTrianglePrinter {
     public RightTrianglePrinter(Printer printer) {
         this.printer = printer;
     }
-
-    // System.out.   --> Console
-    // BufferedWriter --> Console, File
     public String makeALine(int h, int i) {
         return String.format("%s%s\n", "", "*".repeat(i));
-    }
-
-    // 출력하기
-    public void printToFile(String[] lines) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new FileWriter("./aaa.txt"));
-        for (int i = 0; i < lines.length; i++) {
-            bw.append(lines[i]);
-            bw.flush();
-        }
-        bw.close();
-    }
-
-
-
-    // 콘솔에 모양 출력하기
-    public void printToConsole(String[] lines) {
-        for (int i = 0; i < lines.length; i++) {
-            System.out.print(lines[i]);
-        }
     }
 
     // 모양 출력하기
@@ -49,7 +25,7 @@ public class RightTrianglePrinter {
     }
 
     public static void main(String[] args) throws IOException {
-        RightTrianglePrinter rtp = new RightTrianglePrinter(new ConsolePrinter());
+        RightTrianglePrinter rtp = new RightTrianglePrinter(new FilePrinter());
         rtp.printShape(5);
     }
 }
