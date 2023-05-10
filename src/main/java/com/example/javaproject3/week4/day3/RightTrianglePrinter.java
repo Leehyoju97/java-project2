56ff59ed45f781ece608b418bdcbf068c73c6121
Leehyoju97 <dlgywn2420@gmail.com>
2023-05-10 13:35:03 +0900

    feat : 별 콘솔에 출력

--- src/main/java/com/example/javaproject3/week4/day3/RightTrianglePrinter.java
+++ src/main/java/com/example/javaproject3/week4/day3/RightTrianglePrinter.java
@@ -12,16 +12,37 @@ public class RightTrianglePrinter {
         return String.format("%s%s\n", "", "*".repeat(i));
     }
 
-    public void printToFile() throws IOException {
+    // 출력하기
+    public void printToFile(String[] lines) throws IOException {
         BufferedWriter bw = new BufferedWriter(new FileWriter("./aaa.txt"));
         bw.append("aaa");
         bw.flush();
         bw.close();
     }
 
+
+    // 콘솔에 모양 출력하기
+    public void printToConsole(String[] lines) {
+        for (int i = 0; i < lines.length; i++) {
+            System.out.print(lines[i]);
+        }
+    }
+
+    // 모양 출력하기
+    public void printShape(int h) {
+        // 모양 만들기
+        String[] lines = new String[h];
+        for (int i = 0; i < h; i++) {
+            lines[i] = makeALine(h, i + 1);
+        }
+
+        // 모양 출력하기
+        printToConsole(lines);
+    }
+
     public static void main(String[] args) throws IOException {
         RightTrianglePrinter rtp = new RightTrianglePrinter();
-        rtp.printToFile();
+        rtp.printShape(5);
     }
 }
 
